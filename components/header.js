3ba8a2ddf253a6e856ddedd3ca236cb191de68b8
Kenneth Auchenberg <kenneth@auchenberg.dk>
2017-12-10 17:57:00 -0800

    Fix mobile nav

--- components/header.js
+++ components/header.js
@@ -2,19 +2,22 @@ export default () => (
     <header className='header'>
 
       <div className="container">
-        <nav className="navbar navbar-expand-md">
+        <nav className="navbar navbar-dark navbar-expand-md">
           <a className="navbar-brand" href="/">
             <img className="logo" src="/static/images/logo.svg" />
           </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <ul className=" navbar-nav flex-row ml-md-auto d-none d-md-flex">
+          <div class="navbar-collapse collapse" id="collapsingNavbar">
+          <ul className="navbar-nav ml-md-auto d-md-flex">
             <li className="nav-item font-bitblox"><a className="nav-link" href="/tickets">Tickets</a></li>
             <li className="nav-item font-bitblox"><a className="nav-link" href="/#theme">Theme</a></li>
             <li className="nav-item font-bitblox"><a className="nav-link" href="#organizers">About</a></li>
           </ul>
+          </div>
 
         </nav>
       </div>
--- pages/_document.js
+++ pages/_document.js
@@ -2,25 +2,31 @@ import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-	render() {
-		return (
-			<html>
-				<Head>
+    render() {
+        return (
+            <html>
+                <Head>
                     <title>ColdFront 2018, the future of interface</title>
-					<meta name="viewport" content="width=device-width,initial-scale=1"/>
-					<meta name="theme-color" content="#673ab7" />
+                    <meta name="viewport" content="width=device-width,initial-scale=1"/>
+                    <meta name="theme-color" content="#673ab7" />
                     <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400" rel="stylesheet" />
-					<link href="/static/css/bootstrap.min.css" rel="stylesheet"  />
-					<link href="/static/css/page.css" rel="stylesheet"  />
-					<link rel="shortcut icon" href="/static/images/favicon.png" sizes="16x16 32x32 64x64 128x128 256x256" />
-				</Head>
-				<body>
+                    <link rel="stylesheet" type="text/css" href='https://css.tito.io/v1.1' />
+                    <link href="/static/css/bootstrap.min.css" rel="stylesheet"  />
+                    <link href="/static/css/page.css" rel="stylesheet"  />
+                    <link rel="shortcut icon" href="/static/images/favicon.png" sizes="16x16 32x32 64x64 128x128 256x256" />
+                </Head>
+                <body>
                     <div className="page container-fluid px-sm-4 px-md-0">
-    					<Main />
+                        <Main />
                     </div>
-					<NextScript />
-				</body>
-			</html>
-		);
-	}
+                    <NextScript />
+
+                    <script src='https://js.tito.io/v1' async></script>
+                    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>                    
+                </body>
+            </html>
+        );
+    }
 }
