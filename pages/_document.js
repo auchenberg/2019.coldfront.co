0de36fc5a36362972af37ad36ac6f576b433240d
Kenneth Auchenberg <kenneth@auchenberg.dk>
2018-02-16 16:37:04 -0800

    Add new font

--- pages/_document.js
+++ pages/_document.js
@@ -25,6 +25,8 @@ export default class MyDocument extends Document {
                     <meta name="viewport" content="width=device-width,initial-scale=1"/>
                     <meta name="theme-color" content="#673ab7" />
                     <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css?family=Barlow:200,400,700" rel="stylesheet" />
+
                     <link rel="stylesheet" type="text/css" href='https://css.tito.io/v1.1' />
                     <link href="/static/css/bootstrap.min.css" rel="stylesheet"  />
                     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet" />
@@ -40,7 +42,7 @@ export default class MyDocument extends Document {
                     <script src='https://js.tito.io/v1' async></script>
                     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>                    
+                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
                 </body>
             </html>
         );
