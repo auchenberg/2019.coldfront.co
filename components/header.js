a0e7853efac1fc2ac1afb274af2676dbab95826f
Kenneth Auchenberg <kenneth@auchenberg.dk>
2018-03-25 19:10:23 -0700

    New navigation

--- components/header.js
+++ components/header.js
@@ -15,7 +15,7 @@ export default () => (
             <div class="navbar-collapse collapse" id="collapsingNavbar">
                 <ul class="navbar-nav">
                     <li className="nav-item"><a className="nav-link" href="/why-coldfront">Why ColdFront?</a></li>
-                    <li className="nav-item "><a className="nav-link" href="/tickets">Tickets</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/tickets">Tickets</a></li>
                     <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
                     <li className="nav-item"><a className="nav-link" href="/call-for-recommendations">CFR</a></li>
                 </ul>
@@ -31,16 +31,45 @@ export default () => (
           width: 200px;
         }
 
-        .nav-item > a {
-          font-size: 14px;
-          color: #eef9f6;
-        }
         .navbar-dark .navbar-toggler {
             border: 0;
         }
         .navbar-dark .navbar-toggler-icon {
             width: 30px;
             background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='#193EFF' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
+            position: relative;
+            z-index: 1000;
+            cursor: pointer;
+        }
+
+        :global(.navbar-collapse) {
+            transition: transform 300ms;
+            transform: translate3d(0,-100%,0);
+
+            display: block;
+
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            z-index: 900;
+
+            height: 100vh;
+            background-color: rgba(0,0,0,.90);
+
+            text-align: center;
+            padding: 40px;
+        }
+
+        :global(.collapse.show) {
+            transform: translate3d(0,0,0);
+        }
+        .nav-item {
+            font-size: 30px;
+        }
+
+        .nav-link {
+            color: #fff
         }
 
         @media (max-width: 1000px) {
