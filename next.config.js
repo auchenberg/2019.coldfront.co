16d3238f7b069e3504464d825fa7550344174c40
Kenneth Auchenberg <kenneth@auchenberg.dk>
2018-03-25 21:43:32 -0700

    Add static build for Netlify

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+module.exports = {
+    exportPathMap: function () {
+        return {
+            '/': { page: '/' },
+            '/about': { page: '/about' },
+            '/call-for-recommendations': { page: '/call-for-recommendations' },
+            '/tickets': { page: '/tickets' },
+            '/why-coldfront': { page: '/why-coldfront' },
+        }
+    }
+}
