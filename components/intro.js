4e5aca97d04f2f3dc6092856d2870d327d2fe952
Kenneth Auchenberg <kenneth@auchenberg.dk>
2018-03-25 19:11:46 -0700

    New layout for intro component

--- components/intro.js
+++ components/intro.js
@@ -1,53 +1,57 @@
 import NewsletterForm from '../components/newsletterForm'
+import Section from '../components/section'
 
 export default (props) => (
-    <div className="intro d-none d-md-block" style={{ backgroundImage: 'url(' + props.img + ')' }}>
-        <div className="overlay-shadow"></div>
-        <div className="text">
-            <h1>{props.title}</h1>
+
+    <Section>
+        <div className="intro">
+            <div className="row text">
+                <div className="col-md-8">
+                    <h2>{props.title}</h2>
+                    <h1>{props.text}</h1>
+                </div>
+            </div>
+
+            {props.img &&
+            <div className="row">
+                <div className="col-md-12">
+                    <img src={props.img} />
+                </div>
+            </div>
+            }
         </div>
+
     <style jsx>{`
 
-        /* Shadow Overlay */
-        .intro {
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: cover;
-            position: relative;
-            box-shadow: 40px 80px 80px 20px rgba(0, 0, 0, 0.5);
 
-            height: 500px;
-            margin-bottom: -200px;
+        .text {
+            padding-bottom: 50px;
         }
 
         h1 {
-            font-size: 50px;
-
+            font-size: 38px;
         }
 
-        .overlay-shadow {
-            background-image: linear-gradient(to bottom, transparent 0%,rgba(28,28,45,0.80) 30%, #1c1c2d 70%);
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            left: 0;
-            right: 0;
+        h2 {
+            font-size: 24px;
+            font-weight: 600;
         }
 
-        .text {
-            position: absolute;
-            top: 30%;
-            bottom: 0;
-            left: 0;
-            right: 0;
-
-            display: flex;
-            align-items: center;
-            justify-content: center;
+        img {
+            width: 100%;
         }
+
+        @media (max-width: 1000px) {
+            .intro {
+                h1 {
+                    font-size: 20px;
+                }
+            }
+        }
+
 
     `}</style>
-    </div>
+    </Section>
 
 )
 
