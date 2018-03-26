7f3853312967a8413f3bbd92e196543428486654
Kenneth Auchenberg <kenneth@auchenberg.dk>
2018-03-25 19:11:15 -0700

    New styling of list items

--- components/listItem.js
+++ components/listItem.js
@@ -1,56 +1,49 @@
 export default (props) => (
 
-    <li className={ 'list-item ' + (props.size ? 'col-md-' + props.size : 'col-md-6')}>
-        <div className="img" style={{ backgroundImage: 'url(' + props.img + ')' }}></div>
-        <div className="overlay"></div>
+    <li className="list-item">
+
+        <div className="avatarBox">
+            <img className="avatar" src={props.img} />
+        </div>
+
         <div className="text">
-            <h3>{props.title}</h3>
-            <p>{props.body}</p>
+            <div>
+                <h3>{props.title}</h3>
+                <p>{props.body}</p>
+            </div>
         </div>
 
     <style jsx>{`
 
         .list-item {
-            height: 500px;
+            list-style: none;
 
+            strong {
+                font-weight: 800;
+            }
+            .subject {
+                font-weight: 800;
+            }
 
-            display: flex;
-            align-items: flex-end;
-            justify-content: center;
+            .avatarBox {
+                height: 300px;
+                margin-bottom: 30px;
+                overflow: hidden;
+            }
 
-            padding: 20px;
-            position: relative;
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: contain;
+                object-position: left bottom;
+            }
+
+            @media (max-width: 1000px) {
+                .avatarBox {
+                    height: 110px;
+                }
+            }
         }
-
-        .img {
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            left: 0;
-            right: 0;
-            z-index: 1;
-
-            background-repeat: no-repeat;
-            background-size: cover;
-        }
-
-        .overlay {
-            background-image: linear-gradient(to top, #1c1c2d, rgba(28, 28, 45, 0.80) 100%, rgba(108, 28, 45, 0));
-            position: absolute;
-            top: 0;
-            bottom: 0;
-            left: 0;
-            right: 0;
-            z-index: 2;
-        }
-
-        .text {
-            position: relative;
-            z-index: 2;
-            padding: 15px;
-        }
-
-
         `}</style>
     </li>
 )
