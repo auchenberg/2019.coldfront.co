8e1380f40bea4c2eaac57ce1542769ef0d24bc06
Anders Poulsen <and.poulsen@gmail.com>
2018-04-05 14:04:52 +0200

    Right-aligning menu items and allowing user to navigate to frontpage from the menu.

--- components/header.js
+++ components/header.js
@@ -28,6 +28,10 @@ export default () => (
           width: 200px;
         }
 
+        .navbar-brand{
+            z-index: 1000;
+        }
+
         .navbar-dark .navbar-toggler {
             border: 0;
         }
@@ -35,7 +39,6 @@ export default () => (
             width: 30px;
             background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='#193EFF' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
             position: relative;
-            z-index: 1000;
             cursor: pointer;
         }
 
@@ -62,6 +65,10 @@ export default () => (
         :global(.navbar-collapse .navbar-nav){
             transform: translate(0, 25px);
             transition: ease-out 100ms;
+            @media(max-width: 600px){
+                position: relative;
+                top: 3rem;
+            }
         }
 
         :global(.navbar-collapse.show .navbar-nav){
@@ -83,6 +90,7 @@ export default () => (
         }
         .nav-item {
             font-size: 30px;
+            text-align: right;
         }
 
         .nav-link {
