e320f2b92451303aa288de9048103958da1f1718
Kenneth Auchenberg <kenneth@auchenberg.dk>
2018-05-06 20:54:41 -0700

    Make listitems support linking

--- components/listItem.js
+++ components/listItem.js
@@ -1,18 +1,21 @@
 export default (props) => (
 
     <li className="list-item">
-
-        <div className="avatarBox">
-            <img className="avatar" src={props.img} />
-        </div>
-
-        <div className="text">
-            <div>
-                <h3>{props.title}</h3>
-                <p dangerouslySetInnerHTML={{ __html: props.body }}></p>
+        
+            <div className="avatarBox">
+                <a href={props.url} target="_blank">
+                    <img className="avatar" src={props.img} />
+                </a>
             </div>
-        </div>
 
-    <style jsx>{``}</style>
+            <div className="text">
+                <div>
+                    <h3>{props.title}</h3>
+                    <p dangerouslySetInnerHTML={{ __html: props.body }}></p>
+                </div>
+            </div>
+        <style jsx>{`
+        
+        `}</style>
     </li>
 )
