ac6c658bc3f1e1d6d8a12a85009ea3e46b9b4f98
Kenneth Auchenberg <kenneth@auchenberg.dk>
2018-05-25 13:59:27 -0700

    Add Facebook pixel

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { initGA, logPageView } from '../utils/ga'
+import ReactPixel from 'react-facebook-pixel'
 
 import Header from './header'
 import Footer from './footer'
 
 export default class Layout extends React.Component {
+
   componentDidMount () {
     if (!window.GA_INITIALIZED) {
       initGA()
+      ReactPixel.init('592531964472978');
       window.GA_INITIALIZED = true
     }
     logPageView()
+    ReactPixel.pageView();     
   }
   render () {
     return (
