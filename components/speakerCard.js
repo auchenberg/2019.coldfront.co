a736077ce167dff99f192976388fcb161ea40a05
Kenneth Auchenberg <kenneth@auchenberg.dk>
2018-06-02 20:55:34 -0700

    Support markdown

--- components/speakerCard.js
+++ components/speakerCard.js
@@ -3,23 +3,26 @@ import React from 'react';
 import Link from 'next/link'
 import ListItem from '../components/listItem'
 import slug from 'slug'
+import MarkdownIt from 'markdown-it'
 
 class Speaker extends React.Component {
   constructor(props) {
     super(props);
+    this.md = new MarkdownIt();
   }
 
   render(props) {
-
     let body;
     let url;
+    let slugLabel = slug(this.props.speakerName, { lower: true})
+    let description  = this.md.render(this.props.speakerPosition)
 
     if(this.props.compact && this.props.compact == 'true') {
-      url = `/speakers#${slug(this.props.speakerName, { lower: true})}`
+      url = `/speakers#${slugLabel}`
       body = `will cover <strong>${this.props.speakerTheme}.</strong>`;
     } else {
       url = this.props.twitter
-      body = `<span class='title'>${this.props.speakerPosition}</span>.</br></br> will cover <strong>${this.props.speakerTheme}.</strong>`;
+      body = `${description}</br>${this.props.speakerName.split(' ')[0]} will cover <strong>${this.props.speakerTheme}.</strong>`;
     }
 
     return (
@@ -28,6 +31,7 @@ class Speaker extends React.Component {
             body={body}
             img={this.props.speakerImage}
             url={url}
+            anchor={slugLabel}
         />
     );
   }
