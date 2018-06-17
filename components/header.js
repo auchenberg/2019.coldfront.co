2f13363291b6ce690d04f18cb3ec29efc34a57a1
Kenneth Auchenberg <kenneth@auchenberg.dk>
2018-06-17 15:50:05 -0700

    Link to venue

--- components/header.js
+++ components/header.js
@@ -17,6 +17,7 @@ export default () => (
                     <li className="nav-item"><Link href="/speakers"><a className="nav-link">Speakers</a></Link></li>                    
                     <li className="nav-item"><Link href="/community-workshop-day"><a className="nav-link">Workshops and meetups</a></Link></li>                    
                     <li className="nav-item"><Link href="/tickets"><a className="nav-link">Tickets</a></Link></li>
+                    <li className="nav-item"><Link href="/venue-and-practical"><a className="nav-link">Venue + Practicalities</a></Link></li>
                     <li className="nav-item"><Link href="/why-coldfront"><a className="nav-link">Why ColdFront?</a></Link></li>
                     <li className="nav-item"><Link href="/about"><a className="nav-link">About</a></Link></li>
                     <li className="nav-item"><Link href="/call-for-recommendations"><a className="nav-link">CFR</a></Link></li>
