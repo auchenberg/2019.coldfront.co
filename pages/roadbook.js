69255afb641e4701e60a508e81a61b9e4650977d
Kenneth Auchenberg <kenneth@auchenberg.dk>
2018-10-28 20:45:11 -0700

    Add roadbook

---
+++ pages/roadbook.js
@@ -0,0 +1,174 @@
+import Section from '../components/section'
+import Layout from '../components/layout'
+import Intro from '../components/intro'
+
+export default () => (
+    <Layout title="Speaker Roadbook">
+
+        <Section>
+            <Intro
+                title="The Speaker Roadbook for ColdFront 2018"
+                text="The one place with all the practical speaker information"
+                img="/static/images/splash-roadbook.jpg"
+            />
+        </Section>
+        <Section bright>
+
+            <div className="narrow">
+
+                <p>This is the speaker's roadbook. It contains all the important information you as a speaker might want to know before, during and after your stay at ColdFront 2018.</p>
+
+                <p>Please bookmark this page.</p>
+
+                <h3>Contact information</h3>
+
+                <p>Kenneth Auchenberg is your speaker contact during the whole conference. If you have any questions or problems please talk to him. You can also contact Michael at any time during your stay.</p>
+
+                <br />
+
+                <ul>
+                    <li>
+                    <p><strong>Kenneth Auchenberg</strong></p>
+                    <p>Phone: +1 206 355 1131</p>
+                    <p>Email: kenneth@auchenberg.dk</p>
+                    <p>Twitter: @auchenberg</p>
+                    <br />
+                    </li>
+                    <li>
+                    <p><strong>Michael Christensen</strong></p>
+                    <p>Phone: +45 2013 1720</p>
+                    <p>Email: michael@indievid.dk</p>
+                    </li>            
+                </ul>
+
+                <br />
+
+                <h3>Travel to and from the airport</h3>
+
+                <p>If you are flying into Copenhagen you will arrive at and depart from Copenhagen Airport (CPH). The easiest way to get to hotel is by taking the train lines <strong>029 / 1027 / 1029 / 1031 / 1033 / IC</strong> to <strong>Copenhagen Central Station</strong>.</p>
+                <p>When you exit the arrival area in the airport (Terminal 3), simply to continue straight ahead until you see the Train signs. Before going down to the tracks, go the the ticket machines and buy Zone 4 ticket, which takes you down town. The trains departs every 10th minute, so don't worry if you miss a train.</p>
+
+                <p>Please note that Copenhagen don't have Uber/Lyft, and taxies in general are expensive</p>
+                <br />
+                <br />
+
+                <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m24!1m8!1m3!1d36022.131456665644!2d12.558843!3d55.6475434!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x4653ab102787956b%3A0xfc95bcff205ab30b!2sCopenhagen+Airport%2C+Kastrup%2C+Denmark!3m2!1d55.6180236!2d12.650762799999999!4m5!1s0x4652530b33f901ad%3A0x29d4e40f85c727b1!2sSavoy+Hotel%2C+Vesterbrogade+34%2C+1620+K%C3%B8benhavn+V%2C+Denmark!3m2!1d55.6725612!2d12.556468899999999!5e0!3m2!1sen!2sde!4v1503728417357" 
+                    width="900" 
+                    height="600" 
+                    frameborder="0" 
+                    allowfullscreen>
+                </iframe>
+
+                <br />
+                <br />
+
+                <h3>The conference hotel</h3>
+
+                <p>Your hotel is the <strong><a href="http://savoyhotel.dk">Savoy Hotel</a></strong>. The Savoy Hotel is housed in the Løvenborg building and is designed by the architect Anton Rosen, who is also known for the Tuborg building in Hellerup and the Palace Hotel at the Town Hall Square (Rådhuspladsen).</p>
+
+                <p>The Art Nouveau building was built in 1906 and was renamed as a historical monument in 1985 after an extensive restoration work.  The impressive “curtain wall facade”, the first of its kind in Denmark, detail of the building with for example small animal heads on the wall on the inner courtyard, the beautiful lions on the stucco around the elevator and the round windows are all classic Art Nouveau details.</p>
+
+                <p>Random fact: The building was originally the <strong>first in Denmark to have an elevator</strong>. And today, more than 100 years later, the elevator is still working and can be used by hotel guests.</p>
+
+                <br />
+
+                <img src="/static/images/roadbook/savoy.jpg" width="600" />
+
+                <br />
+                <br />
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2249.9447345497174!2d12.554280215943894!3d55.67256118053193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4652530b33f901ad%3A0x29d4e40f85c727b1!2sSavoy+Hotel!5e0!3m2!1sen!2sde!4v1503727840529" 
+                    width="900"
+                    height="600"
+                    frameborder="0" 
+                    allowfullscreen>
+                </iframe>          
+
+                <br />
+                <br />
+
+                <p>The address of the hotel is: <strong>Vesterbrogade 34 1620 København V Denmark</strong> and the phone number of the hotel is: +45 33 26 75 00.</p>
+                <p>Your room is booked in your name and already paid for. Breakfast and WIFI is included.</p>
+
+                <h3>Speaker dinner</h3>
+                
+                <p>At <strong>19:00 on Monday, 12th of November</strong> after our <a href="/prefront"> Prefront</a> event, there will be a speaker dinner. Kenneth will reach out to those of you, who are attending with furher details.</p>
+
+                <h3>The venue</h3>
+
+                <p>ColdFront will be held in VEGA, one of the leading concert stages in Europe. Showcasing original 50's design gives the building a unique atmosphere, and VEGA's decor with dark wood paneling, mahogany floors, friezes, and the many original details including railings, balustrades and lamps in typical Scandinavian style are the hallmarks of VEGA.</p>
+
+                <p>You can read more about the venue on <a href="/venue-and-practical">our venue and practicalities page</a></p>
+                <p>The venue address is: <strong>VEGA, ENGHAVEVEJ 40, 1674 COPENHAGEN</strong>, and is a 15min walk from the hotel</p>
+
+                <p>When entering the venue please go to the front, where the <strong>first row</strong> of the theater is reserved for Speakers and staff.</p>
+
+                <br />
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d4500.142342023371!2d12.545908628889892!3d55.67036252212544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e2!4m5!1s0x4652530b33f901ad%3A0x29d4e40f85c727b1!2sSavoy+Hotel%2C+Vesterbrogade+34%2C+1620+K%C3%B8benhavn+V%2C+Denmark!3m2!1d55.6725612!2d12.556468899999999!4m5!1s0x4652539e80687c13%3A0xf5202e524e3eb86f!2sVega%2C+Enghavevej%2C+K%C3%B8benhavn+V%2C+Denmark!3m2!1d55.66802!2d12.5440612!5e0!3m2!1sen!2sus!4v1503930324099" 
+                    width="900" 
+                    height="600" 
+                    frameborder="0" 
+                    allowfullscreen>
+                </iframe>
+
+                <br />
+                <br />
+
+                <h3>AV / Technical</h3>
+                <p>The projector is a full HD 1080P projector with a 1920x1080 resolution. On stage there will be HDMI + mini-jack connection.</p>
+                <p>We'll provide mini-DP and USB-C adaptors to connect to the projector.</p>
+                
+                <p>If your laptop needs a special adapter to connect to HDMI, please bring one.</p>
+
+                <p>If your presentation requires sound, please let us know before the conference starts so a cable to connect to the PA can be provided for you.</p>
+
+                <p>The venue has WIFI, but with the amount of attendess please prepare your talk and DEMOS as <strong>there's no WIFI.</strong></p>
+
+                <h3>AV testing</h3>
+                <p>We will be at the venue all day on November 12. There will be a option to do AV test on <strong>November 12th from 12:00 to 14:00</strong>, and we highly encourage you to stop by to test your laptop and devices before hand. Alternatively we can test your devices in the breaks during the conference.</p>
+
+                <h3>Your talk</h3>
+
+                <p>Please make yourself familiar with the schedule and the time for your talk. Please be by the stage about 15 minutes before it is your turn to enter the stage. There is a longer break after every second talk. Your speaker slot is <strong>45 minutes</strong>, and it's up to you if you wanna have an Q/A. We got mic's available if needed</p>
+
+                <p>We also encourage you to put your slides online after your talk.</p>          
+
+                <h3>Interactive sessions</h3>
+                <p>For those of you who are hosting interactive sessions, they will take place at our regular conference venue, and the sessions will take place in two tracks: Track 1 at the main stage, and Track 2 in Idealbar, which is a smaller room (150 people) we'll use for  <a href="/prefront"> Prefront</a> too.</p>
+                <p>There will be the same AV connectivity, but the sessions won't be recorded. </p>
+
+                <h3>Workshops / Student Day / Meetups</h3>
+                <p>For those of you who are attending our <a href="/community-workshop-day">community and workshop day</a> will take place on <strong>November 15th 09:30-21:00</strong> hosted at IT University of Copenhagen (ITU), where we have the majority of the university available to us.</p>
+                <p>The workshops will take places in 3 auditoriums, where the attendees will sit, and you have the front with AV and everything. Think typical auditorium setup.</p>
+                    
+                <p>The address of ITU is <a href="https://goo.gl/maps/WoScNoZSUNo">Rued Langgaards Vej 7 2300 København S</a>, and we'll be at the venue from <strong>08:30</strong>.</p>
+                
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2250.6733844686783!2d12.589002215926953!3d55.659889580528564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46525345017412b3%3A0xa280a05485937e38!2sIT-Universitetet+i+K%C3%B8benhavn!5e0!3m2!1sen!2sus!4v1540784259095" width="900" height="450" frameborder="0" allowfullscreen></iframe>
+                <p></p>
+                <p>Doors will open 9:30, where we’ll be starting with workshops at 10:00 for the ColdFront attendees, followed by <a href="/student">ColdFront Student</a>, and in evening with 3 different meetups from the Copenhagen front-end community.</p>
+                <p>You can see the full schedule for the day here on <a href="/community-workshop-day">community and workshop day</a> </p>
+                
+
+                <h3>Filming and streaming</h3>
+                <p>Your talk at the main track will be professionally filmed and published on our <a href="https://www.youtube.com/channel/UCZHbZVjG2wXaLCuHK3LZ5lw">YouTube channel</a> as fast as possible after the conference. All videos published will be published under a CC BY-NC-SA 3.0 licence.</p>
+
+                <p>If you do not want your talk to be filmed and  published, please let us know up front.</p>
+
+                <p>There won't be a livestream.</p>
+
+                <h3>Official hashtag</h3>
+                <p>We feel deeply honored if you mention us in social media. If you do, our official hashtag is: <strong>#coldfront18</strong></p>
+
+ 
+        
+            <style jsx>{`
+                .page-welcome {
+
+                }
+            `}</style>
+            </div>
+        </Section>
+    </Layout>
+)
