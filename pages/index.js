de9973abf3f774167641696b6aceb158593f6c45
Kenneth Auchenberg <kenneth@auchenberg.dk>
2018-11-22 07:21:03 -0800

    Update newsletter

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,14 @@ export default () => (
 
         <Teaser />
 
+        <Section>
+            <div className="newsletter-wrapper narrow">
+                <h3>Don't miss out! Get the latest</h3>
+                <p>We have a huge ColdFront storm planned to arrive in November, so sign up to stay in the loop and get the announcements before everyone else!</p>
+                <NewsletterForm />
+            </div>
+        </Section>
+
         <ProgramOverview />
 
         <SpeakerOverview />
@@ -27,14 +35,6 @@ export default () => (
         <Collaborators />
 
         <Partners />
-
-        <Section>
-            <div className="newsletter-wrapper narrow">
-                <h3>Don't miss out! Get the latest</h3>
-                <p>We have a huge ColdFront storm planned to arrive in November, so sign up to stay in the loop and get the announcements before everyone else!</p>
-                <NewsletterForm />
-            </div>
-        </Section>
         
     <style jsx>{`
 
